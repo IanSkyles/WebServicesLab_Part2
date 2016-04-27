1c170ba8106597d35b3ff5913e1e32e877b04a6f
IanSkyles <ianskyles@gmail.com>
2016-04-27 14:13:04 -0700

    Started on Data lab. Have completed through step 5

---
+++ app/src/main/java/iskyles/tacoma/uw/edu/webservicelab/authenticate/SignInActivity.java
@@ -0,0 +1,15 @@
+package iskyles.tacoma.uw.edu.webservicelab.authenticate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import iskyles.tacoma.uw.edu.webservicelab.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
