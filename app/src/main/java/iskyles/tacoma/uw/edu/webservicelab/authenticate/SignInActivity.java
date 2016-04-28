51ffd7e037683c807f3a04bce4a7e9578fe5fe69
IanSkyles <ianskyles@gmail.com>
2016-04-27 17:17:44 -0700

    Finished through step 15

--- app/src/main/java/iskyles/tacoma/uw/edu/webservicelab/authenticate/SignInActivity.java
+++ app/src/main/java/iskyles/tacoma/uw/edu/webservicelab/authenticate/SignInActivity.java
@@ -1,20 +1,31 @@
 package iskyles.tacoma.uw.edu.webservicelab.authenticate;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import iskyles.tacoma.uw.edu.webservicelab.CourseActivity;
 import iskyles.tacoma.uw.edu.webservicelab.R;
 
 public class SignInActivity extends AppCompatActivity implements LoginFragment.LoginInteractionListener {
 
+    private SharedPreferences mSharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, new LoginFragment() )
+                .commit();
     }
 
     @Override
     public void login(String userId, String pwd){
+        mSharedPreferences.edit().putBoolean(getString(R.string.LOGGEDIN),true).commit();
+        Intent i = new Intent(this, CourseActivity.class);
+        startActivity(i);
+        finish();
 
     }
 }
